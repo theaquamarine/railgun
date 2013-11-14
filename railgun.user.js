9d2c6c806b128787df70b0df3e48b2bbdf1deafa
blue <playbynumbers@gmail.com>
2013-11-14 04:17:57 +0000

    honk

--- railgun.user.js
+++ railgun.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
-// @name		Railgun
-// @namespace	https://github.com/theaquamarine/railgun
-// @homepage	https://github.com/theaquamarine/railgun
-// @description	Fuck MediaWiki
-// @match		http://taimapedia.org/*
-// @version		0.1
-// @updateurl	https://raw.github.com/theaquamarine/railgun/master/railgun.user.js
+// @name        Railgun
+// @namespace   https://github.com/theaquamarine/railgun
+// @homepage    https://github.com/theaquamarine/railgun
+// @description Fuck MediaWiki
+// @match       http://taimapedia.org/*
+// @version     0.1
+// @updateurl   https://raw.github.com/theaquamarine/railgun/master/railgun.user.js
 // ==/UserScript==
 
 var nodes = document.querySelectorAll("a[href*='Special:Block'");
 for(var i = 0; i < nodes.length; i++)
 {
-	var currentNode = nodes[i];
-	currentNode.href += "&wpExpiry=infinite&wpReason=Spamming&wpAutoBlock=1&wpHardBlock=1";
+    var currentNode = nodes[i];
+    currentNode.href += "&wpExpiry=infinite&wpReason=Spamming&wpAutoBlock=1&wpHardBlock=1";
 }
