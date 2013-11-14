ce8de4ef57f8f7ae2ae1f87dff54b6848318011b
blue <playbynumbers@gmail.com>
2013-11-14 03:29:56 +0000

    Railgun v0.1

--- README.txt
+++ README.txt
@@ -1,2 +1,4 @@
 MediaWiki is hell to mod.
 Let's see if I can make it worse.
+
+v0.1 - Basic functionality, preloads "block" button with indefinite (infinite) expiry, reason "Spamming", hard- & autoblocks. Works in Tampermonkey.
--- railgun.js
+++ railgun.js
@@ -1,7 +1,13 @@
-document.querySelectorAll("a[href*='Special:Block'").href += "&wpExpiry=infinite&wpReason=Spamming";
+// ==UserScript==
+// @name		Railgun
+// @namespace	https://github.com/theaquamarine/railgun
+// @description	Fuck MediaWiki
+// @include		http://taimapedia.org/*
+// ==/UserScript==
+
 var nodes = document.querySelectorAll("a[href*='Special:Block'");
 for(var i = 0; i < nodes.length; i++)
 {
 	var currentNode = nodes[i];
-	currentNode.href += "&wpExpiry=infinite&wpReason=Spamming";
+	currentNode.href += "&wpExpiry=infinite&wpReason=Spamming&wpAutoBlock=1&wpHardBlock=1";
 }
