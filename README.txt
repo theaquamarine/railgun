b92819a93765adda4d5e36178b621846a0e4b098
blue <playbynumbers@gmail.com>
2014-02-09 19:04:48 +0000

    Add NewPages & BlockList links to sidebar

--- README.txt
+++ README.txt
@@ -1,6 +1,13 @@
 MediaWiki is hell to mod.
 Let's see if I can make it worse.
 
+Installation:
+Open railgun.user.js in raw mode (ie https://github.com/theaquamarine/railgun/raw/master/railgun.user.js). This should prompt Tampermonkey/whatever to start the install process.
+
+Usage:
+Special:RecentChanges is a good place to start, as is Special:NewPages.
+If you want to block and nuke a user, blocking first is usually the better option: it's easy to nuke blocked users from Special:BlockList, whereas Special:Log/delete doesn't have block links (as of v1.0).
+
 v1.0 - Simplify block log strings, add tools to revision history pages, add tools to :Log/block, bunch of other stuff.
 v0.4 - Organise user tools into separate column.
 v0.3 - Added Nuke link to sidebar.
--- railgun.user.js
+++ railgun.user.js
@@ -10,19 +10,19 @@
 
 
 //neatens up block log entries.
-//encounters strange bugs (getElementsByClassName("blockExpiry").parentNode === null)
-var expiries = document.getElementsByClassName("blockExpiry");
-//for (var i = 0; i < expiries.length; i++) {if (expiries[i].parentNode == null) console.log("true for " + i);}
+//encounters strange bugs (getElementsByClassName('blockExpiry').parentNode === null)
+var expiries = document.getElementsByClassName('blockExpiry');
+//for (var i = 0; i < expiries.length; i++) {if (expiries[i].parentNode == null) console.log('true for '' + i);}
 for (var i = 0; i < expiries.length; i++)
 {
     var expiry = expiries[i];
     if (expiry.nextSibling !== null)
         //really ought to test what's in the next node, but this breaks the script.
-        //&& (expiry.nextSibling == " (account creation disabled) ‎" || expiry.nextSibling == " (account creation disabled) "))
+        //&& (expiry.nextSibling == ' (account creation disabled) ‎' || expiry.nextSibling == ' (account creation disabled) '))
     {
         expiry.parentNode.removeChild(expiry.nextSibling);
     }
-    expiry.textContent = " " + expiry.title + " ";
+    expiry.textContent = ' ' + expiry.title + ' ';
     if (expiry.previousSibling !== null)
     {
         expiry.parentNode.removeChild(expiry.previousSibling);
@@ -30,17 +30,39 @@ for (var i = 0; i < expiries.length; i++)
 }
 
 
-//add a Nuke link to sidebar
-navigationlist = document.getElementById("p-navigation").getElementsByTagName("div")[0].getElementsByTagName("ul")[0];
-var aNode = document.createElement("a");
-var liNode = document.createElement("li");
-aNode.appendChild(document.createTextNode("Nuke"));
-aNode.setAttribute('href',"/index.php?title=Special:Nuke");
+//Sidebar manipulation
+navigationlist = document.getElementById('p-navigation').getElementsByTagName('div')[0].getElementsByTagName('ul')[0];
+//add a Nuke link
+var aNode = document.createElement('a');
+var liNode = document.createElement('li');
+aNode.appendChild(document.createTextNode('Nuke'));
+aNode.setAttribute('title','Delete all of a user or IP address\'s pages');
+aNode.setAttribute('href','/index.php?title=Special:Nuke');
 liNode.appendChild(aNode);
-liNode.className = 'plainlinks';
+liNode.setAttribute('id','railgun-nuke');
 navigationlist.appendChild(liNode);
 
-var nodes = document.getElementsByClassName("mw-usertoollinks");
+//add a NewPages link
+var aNode = document.createElement('a');
+var liNode = document.createElement('li');
+aNode.appendChild(document.createTextNode('New Pages'));
+aNode.setAttribute('title','Show all new pages');
+aNode.setAttribute('href','/index.php?title=Special:NewPages');
+liNode.appendChild(aNode);
+liNode.setAttribute('id','railgun-newpages');
+navigationlist.appendChild(liNode);
+
+//add a BlockList link
+var aNode = document.createElement('a');
+var liNode = document.createElement('li');
+aNode.appendChild(document.createTextNode('Block List'));
+aNode.setAttribute('title','List all blocked users & IPs');
+aNode.setAttribute('href','/index.php?title=Special:BlockList');
+liNode.appendChild(aNode);
+liNode.setAttribute('id','railgun-blocklist');
+navigationlist.appendChild(liNode);
+
+var nodes = document.getElementsByClassName('mw-usertoollinks');
 for(var i = 0; i < nodes.length; i++)
 {
     var usertools = nodes[i];
@@ -48,31 +70,31 @@ for(var i = 0; i < nodes.length; i++)
     var tailnode = usertools.lastChild;
     var newnode;
 
-    //make a "block for spam" node
-    if (spblocknode.textContent == "block") //no interest in anyone with a "change block" or "contribs" button.
+    //make a 'block for spam' node
+    if (spblocknode.textContent == 'block') //no interest in anyone with a 'change block' or 'contribs' button.
     {
-        usertools.insertBefore(document.createTextNode(" | "),tailnode);
+        usertools.insertBefore(document.createTextNode(' | '),tailnode);
         newnode = spblocknode.cloneNode();
-        newnode.href += "&wpExpiry=infinite&wpReason=Spamming&wpAutoBlock=1&wpHardBlock=1";
-        newnode.textContent = "block for spam";
+        newnode.href += '&wpExpiry=infinite&wpReason=Spamming&wpAutoBlock=1&wpHardBlock=1';
+        newnode.textContent = 'block for spam';
         usertools.insertBefore(newnode,tailnode);
     }
 
-    if (spblocknode.textContent == "block" || spblocknode.textContent == "contribs")
+    if (spblocknode.textContent == 'block' || spblocknode.textContent == 'contribs')
     {
-        //make a "nuke" node
-        usertools.insertBefore(document.createTextNode(" | "),tailnode);
+        //make a 'nuke' node
+        usertools.insertBefore(document.createTextNode(' | '),tailnode);
         newnode = spblocknode.cloneNode();
-        newnode.href = newnode.href.replace("Special:Block","Special:Nuke");
-        newnode.title = newnode.title.replace("Special:Block","Special:Nuke");
-        newnode.href = newnode.href.replace("Special:Contributions","Special:Nuke");
-        newnode.title = newnode.title.replace("Special:Contributions","Special:Nuke");
-        newnode.textContent = "nuke";
+        newnode.href = newnode.href.replace('Special:Block','Special:Nuke');
+        newnode.title = newnode.title.replace('Special:Block','Special:Nuke');
+        newnode.href = newnode.href.replace('Special:Contributions','Special:Nuke');
+        newnode.title = newnode.title.replace('Special:Contributions','Special:Nuke');
+        newnode.textContent = 'nuke';
         usertools.insertBefore(newnode,tailnode);
     }
 }
 
-var nodes = document.getElementsByClassName("mw-usertoollinks");
+var nodes = document.getElementsByClassName('mw-usertoollinks');
 for(var i = 0; i < nodes.length; i++)
 {
     var usertools = nodes[i];
@@ -83,18 +105,18 @@ for(var i = 0; i < nodes.length; i++)
 //split up user account lines to ensure toolbox links occur in columns -> easier bulk clicking
 
 //mods table headers to span two columns. run this first, less visually distracting.
-var tablepreheaders = document.getElementsByClassName("mw-rc-unwatched");   //span class="mw-rc-unwatched"
+var tablepreheaders = document.getElementsByClassName('mw-rc-unwatched');   //span class='mw-rc-unwatched'
 for (var i = 0; i < tablepreheaders.length; i++)
 {
     var tableheader = tablepreheaders[i].parentNode;
     //&nbsps are to fix padding on previous cell (comments). Would be better fixed using actual padding/margins.
-    tableheader.outerHTML = tableheader.outerHTML.replace("<td><span class=\"mw-rc-unwatched\">","<td colspan=\"2\"><span class=\"mw-rc-unwatched\">");
+    tableheader.outerHTML = tableheader.outerHTML.replace('<td><span class=\'mw-rc-unwatched\'>','<td colspan=\'2\'><span class=\'mw-rc-unwatched\'>');
 }
 
 //splits user lines into two cells
-var rcuserlines = document.getElementsByClassName("mw-enhanced-rc-nested"); //td class="mw-enhanced-rc-nested"
+var rcuserlines = document.getElementsByClassName('mw-enhanced-rc-nested'); //td class='mw-enhanced-rc-nested'
 for(var i = 0; i < rcuserlines.length; i++)
 {
     rcuserline = rcuserlines[i].parentNode;
-    rcuserline.innerHTML = rcuserline.innerHTML.replace("<span class=\"mw-usertoollinks\">","&nbsp&nbsp</td><td><span class=\"mw-usertoollinks\">");
+    rcuserline.innerHTML = rcuserline.innerHTML.replace('<span class=\'mw-usertoollinks\'>','&nbsp&nbsp</td><td><span class=\'mw-usertoollinks\'>');
 }
